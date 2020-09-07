b3adc2f234c62cf31e4137ea75348e204a79bd66
Ignacio <igna.lucas1@gmail.com>
2020-09-07 20:56:15 -0300

    un paso mas

--- ProjectoListas/apps/lista/models.py
+++ ProjectoListas/apps/lista/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+# estos son los clases y funciones principales
+
+class Lista(models.Model):
+	usuario = models.ForeignKey( ,on_delete=models.CASCADE)
+	idLista = models.AutoField(primary_key=True)
+	nombreLista = models.CharField(max_length=30)
+	linea = models.AutoField()
+	contenido = models.CharField(max_length=30)
+
+def __str__(self):
+	return self.nombreLista
+
+
+
+
+
+
+
+class Empresa(models.Model):
+	idEmpresa = models.AutoField(primary_key=True)
+	nombreEmpresa = models.CharField(max_length=50)
+
+
